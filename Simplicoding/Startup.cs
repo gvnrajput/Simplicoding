1fab3c0a54241b4014abf0b5482e1c04e174498c
Gaurav <81220628+gvnrajput@users.noreply.github.com>
2022-06-26 22:45:29 +0530

    Update Startup.cs

--- Simplicoding/Startup.cs
+++ Simplicoding/Startup.cs
@@ -24,7 +24,7 @@ namespace Simplicoding
         {
             services.AddRazorPages();
         }
-fdhfdhgfhjgfj
+jhghfghjfhfhgfhg
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
